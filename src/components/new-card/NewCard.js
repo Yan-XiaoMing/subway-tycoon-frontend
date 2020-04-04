c51d15d117c6e8a042998ebc7eb64dabe923f757
YanQiMing <1024744235@qq.com>
2020-04-04 16:25:35 +0800

    增加卡片和基本样式,卡片描述待完善

--- src/components/new-card/NewCard.js
+++ src/components/new-card/NewCard.js
@@ -4,16 +4,15 @@ import './style.less';
 const NewCard = (props) => {
   return (
     <div className="new-card-container new-card-container-list">
+      <img src={require('../../assets/img/card_bg01.jpg')} className="new-card-bg"/>
       <div className="new-card-svg">
         <img src={require('../../assets/img/new_tag.svg')} alt="new"/>
       </div>
       <div className="new-card-wrapper">
         <div className="new-card-title">
-          <div className="new-card-title-svg">
-            <svg className="icon" aria-hidden="true">
-              <use xlinkHref="#iconhangzhouditie"/>
-            </svg>
-          </div>
+          <svg className="icon subway-logo" aria-hidden="true">
+            <use xlinkHref="#iconhangzhouditie"/>
+          </svg>
           <span className="new-card-title-item">凤起路</span>
         </div>
       </div>
